942d66cc68f57fa884166a87c7c0c36074952683
Will Carter <will.carter@fergusllc.com>
2020-04-20 13:28:19 -0600

    topic form

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -15,6 +15,7 @@ class ClassroomsController < ApplicationController
 
   # GET /classrooms/new
   def new
+    console
     @classroom = Classroom.new
   end
 
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,4 @@
 class Course < ApplicationRecord
   belongs_to :classroom
+  has_many :topics
 end

fc01dabf3371b5389a5477ef9a05a83543209006
Will Carter <will.carter@fergusllc.com>
2020-04-28 09:28:44 -0600

    delete course, cascade destroy works

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ApplicationRecord
+  has_many :interactions, dependent: :destroy
+  has_many :course_people
   has_many :students, through: :course_person
   has_many :teachers, through: :course_person
 end

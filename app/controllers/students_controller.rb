0584c4f9c88f5c31b5c6a2f4d0fb75168108a478
Will Carter <will.carter@fergusllc.com>
2020-05-02 17:38:56 -0600

    refactor

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -47,7 +47,7 @@ class StudentsController < ApplicationController
     
     respond_to do |format|
       if @student.update(student_params)
-        format.html { redirect_to students_url, notice: 'Student was successfully updated.' }
+        format.html { redirect_to @student, notice: 'Student was successfully updated.' }
         format.json { render :show, status: :ok, location: @student }
       else
         format.html { render :edit }

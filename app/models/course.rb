28e9ab4ed6a8e72fc6d7c7ed3753e5829076c7d2
Will Carter <will.carter@fergusllc.com>
2020-05-06 17:41:20 -0600

    course show teachers and students

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,5 +3,5 @@ class Course < ApplicationRecord
     has_many :interactions, dependent: :destroy
     
     has_many :course_people
-    has_many :people, :through => :course_people, as: :students
+    has_many :people, :through => :course_people
 end

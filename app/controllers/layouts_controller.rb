54a1a02622df9385dd57c3cd297fca0ac2210e06
Will Carter <will.carter@fergusllc.com>
2020-05-14 10:55:38 -0600

    remove binding.pry

--- app/controllers/layouts_controller.rb
+++ app/controllers/layouts_controller.rb
@@ -2,37 +2,27 @@ class LayoutsController < ApplicationController
 
   def index
 
-    binding.pry
-
     if logged_in?
     
       @interactions = []
 
-      binding.pry
-  
       # teachers create interactions
       current_user.interactions.each do |interaction|
         @interactions.push interaction  
       end
       
-      binding.pry
-  
       # students have interactions through their courses
       current_user.courses.each do |course|
         if !course.interactions.empty?
           course.interactions.each do |interaction|
-            @interactions.push(interaction)
+            @interactions.push interaction unless @interactions.include?(interaction)
           end
         end
       end
   
-      binding.pry
     else
       redirect_to welcome_path
     end
-    
-   
-
   end
     
   def welcome

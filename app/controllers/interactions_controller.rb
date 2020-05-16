d22afaafe84bb9d9089b67fe9fce1de30d97cc51
Will Carter <will.carter@fergusllc.com>
2020-05-16 16:27:55 -0600

    refactor

--- app/controllers/interactions_controller.rb
+++ app/controllers/interactions_controller.rb
@@ -3,14 +3,11 @@ class InteractionsController < ApplicationController
 
   def index
     @index_title = "Interactions"
-    @new_path = new_interaction_path
     @interactions = if request.path.include?("announcements")
                       @index_title = "Announcements"
-                      @new_path = new_announcement_path
                       Announcement.all
                     elsif request.path.include?("assignments")
                       @index_title = "Assignments"
-                      @new_path = new_assignment_path
                       Assignment.all
                     else
                       Interaction.all #TODO: necessary?

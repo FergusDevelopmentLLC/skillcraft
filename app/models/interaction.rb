41e48945645c924287ad991e1be59639351dc755
Will Carter <will.carter@fergusllc.com>
2020-05-20 13:31:07 -0600

    refactor

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -2,4 +2,9 @@ class Interaction < ApplicationRecord
   belongs_to :course
   belongs_to :user
   has_many :responses, dependent: :destroy
+
+  def responses_type
+    responses.first.type.pluralize.split(/(?=[A-Z])/).join(' ') unless responses.empty?
+  end
+
 end

75627cace6d1a670e146d88407ff8fe28396bc99
Will Carter <will.carter@fergusllc.com>
2020-06-17 10:12:00 -0600

    added pg db for heroku deployment

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+#require 'dotenv' if ENV['RACK_ENV'] != 'production'
+#Dotenv.load if ENV['RACK_ENV'] != 'production'

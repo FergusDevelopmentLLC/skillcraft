e586906365361be133af20efac1c64a9084025fa
Will Carter <will.carter@fergusllc.com>
2020-06-21 16:13:48 -0600

    refactor omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
+require "pry"
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   #binding.pry
-  # provider :twitter, Rails.application.secrets.omniauth_provider_key, Rails.application.secrets.omniauth_provider_secret
-  provider :twitter, ENV["OMNIAUTH_PROVIDER_KEY"], ENV["OMNIAUTH_PROVIDER_SECRET"]
+  provider :twitter, Rails.application.secrets.omniauth_provider_key, Rails.application.secrets.omniauth_provider_secret
+  # provider :twitter, ENV["OMNIAUTH_PROVIDER_KEY"], ENV["OMNIAUTH_PROVIDER_SECRET"]
 end
